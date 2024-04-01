9d1ea11b8980d7eb2f08ee93d8064402b620bb2d
prizhimin <prizhimin@gmail.com>
2024-04-01 22:54:10 +0500

    2 commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from .models import UserApp
 
-# Create your views here.
 
 @login_required
 def dashboard(request):
