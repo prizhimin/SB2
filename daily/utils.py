184f8d2f78d6bb9122c91d604aea0215da4bac9a
prizhimin <prinv@yandex.ru>
2024-04-04 15:53:16 +0500

    get_date_for_report - отключен тестовый вывод

--- daily/utils.py
+++ daily/utils.py
@@ -14,17 +14,17 @@ def get_date_for_report():
     if current_datetime.weekday() in [5, 6] or (current_datetime.weekday() == 0 and
                                                 current_datetime.time() < timezone.datetime.strptime('09:30',
                                                                                                      '%H:%M').time()):
-        print('get_date_for_report: Сейчас суббота, воскресенье или понедельник до 09:30')
+        # print('get_date_for_report: Сейчас суббота, воскресенье или понедельник до 09:30')
         # Если условие выполняется, выбираем дату прошлой пятницы
         return (current_datetime.date()
                 - timezone.timedelta(days=current_datetime.weekday())
                 + timezone.timedelta(days=4, weeks=-1))
     else:
         if current_datetime.weekday() == 0:
-            print('get_date_for_report: Сейчас понедельник после 09:30')
+            # print('get_date_for_report: Сейчас понедельник после 09:30')
             # Если сейчас понедельник и время больше 09:30, то возвращаем сегодняшнюю дату
             return current_datetime.date()
         else:
             # Иначе возвращаем вчерашнюю дату
-            print('get_date_for_report: Возвращаем вчерашнюю дату')
+            # print('get_date_for_report: Возвращаем вчерашнюю дату')
             return current_datetime.date() - timezone.timedelta(days=1)
