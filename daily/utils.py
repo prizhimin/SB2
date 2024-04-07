b6b88bec449d217cc88f8dc68a69c2e57832d95e
prizhimin <prizhimin@gmail.com>
2024-04-07 11:38:41 +0500

    get_date_for_report правильно возвращает дату

--- daily/utils.py
+++ daily/utils.py
@@ -4,27 +4,28 @@ from django.utils import timezone
 def get_date_for_report():
     """
     Функция предлагает дату для отчётов исходя из дня недели
-    Если сегодня - суббота, воскресенье или понедельник до 09:30, то будет возвращена дата прошлой пятницы
+    Если сегодня - суббота, воскресенье или понедельник до 09:30, то будет возвращена дата последней пятницы
     Если сегодня - понедельник после 09:30, то вернётся текущая дата, иначе вернётся вчерашняя дата
     """
-    # Определяем текущую дату и время
+    # Определяем текущую дату и время МСК
     current_datetime = timezone.now().astimezone(tz=timezone.get_current_timezone())
-    print(current_datetime)
-    # Проверяем, что сейчас суббота, воскресенье или понедельник, и время меньше 09:30
-    if current_datetime.weekday() in [5, 6] or (current_datetime.weekday() == 0 and
-                                                current_datetime.time() < timezone.datetime.strptime('09:30',
-                                                                                                     '%H:%M').time()):
-        # print('get_date_for_report: Сейчас суббота, воскресенье или понедельник до 09:30')
-        # Если условие выполняется, выбираем дату прошлой пятницы
-        return (current_datetime.date()
-                - timezone.timedelta(days=current_datetime.weekday())
-                + timezone.timedelta(days=4, weeks=-1))
-    else:
-        if current_datetime.weekday() == 0:
-            # print('get_date_for_report: Сейчас понедельник после 09:30')
-            # Если сейчас понедельник и время больше 09:30, то возвращаем сегодняшнюю дату
-            return current_datetime.date()
-        else:
-            # Иначе возвращаем вчерашнюю дату
-            # print('get_date_for_report: Возвращаем вчерашнюю дату')
+    match current_datetime.weekday():
+        case 5 | 6:
+            # суббота или воскресенье - возращаем дату пятницы этой недели
+            return (current_datetime.date()
+                    - timezone.timedelta(days=current_datetime.weekday())
+                    + timezone.timedelta(days=4))
+        case 0:
+            # понедельник
+            if current_datetime.time() < timezone.datetime.strptime('09:30', '%H:%M').time():
+                # время меньше 09:30 МСК
+                # возвращаем дату пятницы прошлой недели
+                return (current_datetime.date()
+                        - timezone.timedelta(days=current_datetime.weekday())
+                        + timezone.timedelta(days=4, weeks=-1))
+            else:
+                # Если сейчас понедельник и время больше 09:30, то возвращаем сегодняшнюю дату
+                return current_datetime.date()
+        case _:
+            # Возвращаем вчерашнюю дату
             return current_datetime.date() - timezone.timedelta(days=1)
