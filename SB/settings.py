39937ecb44c07dcf340e03fda1cf3a001fdeaa9d
prizhimin <prinv@yandex.ru>
2024-04-11 12:00:00 +0500

    ***

--- SB/settings.py
+++ SB/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'SB.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoproject',
+        'USER': 'djangouser',
+        'PASSWORD': 'Qsfjro!BIBb748489sdс',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -137,4 +148,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'static/css'),
+    os.path.join(BASE_DIR, 'static/admin/css')
 ]
