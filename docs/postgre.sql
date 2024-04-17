f4961e4ec2a5528626c1ee281be83325d6b7dc4a
Nikolay Prizhimin <prinv@yandex.ru>
2024-04-17 06:51:04 +0500

    SQL

--- docs/postgre.sql
+++ docs/postgre.sql
@@ -1,4 +1,4 @@
-sudo -u postgres psql
+-- sudo -u postgres psql
 CREATE DATABASE djangoproject;
 CREATE USER djangouser WITH PASSWORD '1234';
 ALTER ROLE djangouser SET client_encoding TO 'utf8';
@@ -6,4 +6,4 @@ ALTER ROLE djangouser SET default_transaction_isolation TO 'read committed';
 ALTER ROLE djangouser SET timezone TO 'UTC';
 GRANT ALL PRIVILEGES ON DATABASE djangoproject TO djangouser;
 ALTER DATABASE djangoproject OWNER TO djangouser;
-\q
+-- \q
