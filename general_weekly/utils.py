cd813ed5b8327b4d3d89a2c62d2010cd501675bb
prizhimin <prinv@yandex.ru>
2024-04-18 14:00:21 +0500

    Фильтр еженедельных отчётов по охране работает

--- general_weekly/utils.py
+++ general_weekly/utils.py
@@ -1,15 +1,12 @@
 from datetime import datetime, timedelta
 
 
-def friday_of_week(date_string):
+def friday_of_week(date):
     """
     Функция возвращает дату пятницы
     :param date_string: дата дня недели
     :return: дата пятницы этой недели
     """
-    # Преобразуем строку с датой в объект datetime
-    date_format = '%d.%m.%Y'
-    date = datetime.strptime(date_string, date_format)
     # Вычисляем день недели (0 - понедельник, 6 - воскресенье)
     weekday = date.weekday()
     # Вычисляем смещение для пятницы (если сегодня пятница, то смещение равно 0,
@@ -18,5 +15,5 @@ def friday_of_week(date_string):
     # Вычисляем дату пятницы добавлением смещения к исходной дате
     friday_date = date + timedelta(days=friday_offset)
     # Форматируем дату пятницы в строку с тем же форматом
-    return friday_date.strftime(date_format)
+    return friday_date
 
--- general_weekly/views.py
+++ general_weekly/views.py
@@ -1,14 +1,41 @@
 from django.shortcuts import render, get_object_or_404, redirect
-# from datetime import timedelta
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-# from django.utils import timezone
+from .models import WeeklyUserDepartment, WeeklyReport, WeeklyCreatorsSummaryReport
+from .utils import friday_of_week
+from commondata.forms import DateForm
+from datetime import datetime
 # from django.apps import apps
 # from shutil import copy
 # import os
 # from openpyxl import load_workbook
-
+# from datetime import timedelta
 
 @login_required
 def general_weekly(request):
-    return render(request, 'general_weekly/report_list.html')
+    # Получаем текущего пользователя
+    user = request.user
+    # Получаем филиалы, к которым пользователь имеет отношение
+    user_departments = WeeklyUserDepartment.objects.filter(user=user).values_list('department', flat=True)
+    # Получаем все отчеты, связанные с этими филиалами, и сортируем их по дате отчета
+    reports = WeeklyReport.objects.filter(department__in=user_departments).order_by('-report_date')
+    # Если форма отправлена методом POST
+    if request.method == 'POST':
+        form = DateForm(request.POST)
+        if form.is_valid():
+            selected_date = form.cleaned_data['selected_date']
+            # Фильтруем отчеты по выбранной дате
+            reports = reports.filter(report_date=friday_of_week(selected_date))
+    else:
+            form = DateForm(initial={'selected_date': friday_of_week(datetime.now())})
+
+    for report in reports:
+        report.user_full_name = f"{report.author.first_name} {report.author.last_name}"
+    summary_reports_creators = []
+    first_summary_report = WeeklyCreatorsSummaryReport.objects.first()
+    if first_summary_report:
+        summary_reports_creators = [user.username for user in first_summary_report.creators.all()]
+    return render(request, 'general_weekly/report_list.html',
+                  {'reports': reports,
+                   'form': form,
+                   'summary_reports_creators': summary_reports_creators})
