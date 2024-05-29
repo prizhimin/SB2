4fa48ef868689c5407a1cb66e6ad178468cd2072
prizhimin <prinv@yandex.ru>
2024-05-29 12:26:51 +0500

    Создано приложение "Служебные проверки"

--- docs/set_permissions.sh
+++ docs/set_permissions.sh
@@ -5,6 +5,7 @@ find /srv/SB2 -type d -exec chmod 755 {} \;
 [ -d /srv/SB2/daily/reports ] || mkdir -p /srv/SB2/daily/reports
 [ -d /srv/SB2/general_weekly/reports ] || mkdir -p /srv/SB2/general_weekly/reports
 [ -d /srv/SB2/venv ] || tar -xzvf /var/backups/django/venv.tar.gz -C /srv/SB2
+[ -d /srv/SB2/media ] || mkdir -p /srv/SB2/media
 chmod 775 /srv/SB2/daily/reports/
 chmod 775 /srv/SB2/general_weekly/reports/
 chown www-data:www-data -R /srv/SB2
