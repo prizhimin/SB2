41e56fb8def4f0f8b7225837f45d56a3ed27908b
prizhimin <prinv@yandex.ru>
2024-08-07 14:31:40 +0500

    Создание папки investigations/reports

--- docs/set_permissions.sh
+++ docs/set_permissions.sh
@@ -5,9 +5,11 @@ find /srv/SB2 -type d -exec chmod 755 {} \;
 [ -d /srv/SB2/daily/reports ] || mkdir -p /srv/SB2/daily/reports
 [ -d /srv/SB2/general_weekly/reports ] || mkdir -p /srv/SB2/general_weekly/reports
 [ -d /srv/SB2/sixmonths2024/reports ] || mkdir -p /srv/SB2/sixmonths2024/reports
+[ -d /srv/SB2/investigations/reports ] || mkdir -p /srv/SB2/investigations/reports
 [ -d /srv/SB2/venv ] || tar -xzvf /var/backups/django/venv.tar.gz -C /srv/SB2
 [ -d /srv/SB2/media ] || mkdir -p /srv/SB2/media
 chmod 775 /srv/SB2/daily/reports/
 chmod 775 /srv/SB2/general_weekly/reports/
+chmod 775 /srv/SB2/investigations/reports/
 chmod 775 /srv/SB2/sixmonths2024/reports/
 chown www-data:www-data -R /srv/SB2
